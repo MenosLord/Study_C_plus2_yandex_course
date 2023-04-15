0d431e297fb27f84ed22c77e0bdcaaf0b65a3212
MenosLord <lordmalsagov@gmail.com>
2023-04-15 19:43:03 +0300

    add 5_app.cpp

---
+++ 1_chapter/1_app/5_app.cpp
@@ -0,0 +1,12 @@
+// Дано двузначное число. Выведите его первую цифру (число десятков)
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int a = 0;
+    cin >> a;
+    cout << a / 10;
+    return 0;
+}
