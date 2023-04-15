216a488724b0ac6efc2707083cd18483325f41b9
MenosLord <lordmalsagov@gmail.com>
2023-04-15 19:35:25 +0300

    add easy programm

---
+++ 1_chapter/1_app/2_app.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+// N белочек нашли K орешков и решили разделить их поровну. Определите, сколько орешков достанется каждой белочке.
+
+using namespace std;
+int main() {
+    int a = 0, b = 0;
+    cin >> a >> b;
+    cout << b / a;
+    return 0;
+}
